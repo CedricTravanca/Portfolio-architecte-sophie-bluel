9e5bfd4f2800c53315465024ba7890a9b596414c
Cedric Travanca <cedric.travanca@gmail.com>
2023-09-08 15:54:33 +0200

    recup du token afin de verifier si on peut se connecter et redirection ou message d'erreur

--- FrontEnd/script2.js
+++ FrontEnd/script2.js
@@ -1,12 +1,17 @@
-console.log("coucou")
+// si le token n'est pas vide, nous pouvons acceder à la page
+if(localStorage.getItem("tokenIdentification") != null) {
+    window.location.href = "http://127.0.0.1:5500"
+}
+
 // On empêche le rechargement de la page
 let formulaire = document.getElementById("form")
 formulaire.addEventListener("submit", (event) => {
     event.preventDefault()
+// Nous définissions les valeurs de l'identifiant et du mdp.
     let user = document.getElementById("e-mail").value
     let password = document.getElementById("mot-de-passe").value
     console.log(user, password)
-    
+// Nous envoyons à l'api les infos utilisateur et mdp pour verification
     fetch("http://localhost:5678/api/users/login",{
         method: 'POST',
         headers: {
@@ -16,7 +21,30 @@ formulaire.addEventListener("submit", (event) => {
             "email": user,
             "password": password
         })
+    
+}).then((response)=>{
+    console.log(response)
+    response.json().then((data)=>{
+        console.log(data)
+        if(data.token != null){
+            localStorage.setItem("tokenIdentification",data.token)
+            window.location.href = "http://127.0.0.1:5500" 
+            
+            
+        }else {
+            alert ("Identifiant ou mot de passe incorrect")
+        }
+
     })
 })
+    
 
+    //
 
+ /*   if (response.ok) {
+        document.location.href='http://127.0.0.1:5500/'
+
+    } else {
+        throw new Error("Identifiant ou Mot de passe incorrect")
+    }*/
+})
