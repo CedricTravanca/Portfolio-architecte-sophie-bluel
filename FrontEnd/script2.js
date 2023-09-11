2c08c86ddd765a31b055b299298b925c1bc7c559
Cedric Travanca <cedric.travanca@gmail.com>
2023-09-11 16:58:52 +0200

    je n'avais pas sauvegarder suite a selection de la barre noir en JS

--- FrontEnd/script2.js
+++ FrontEnd/script2.js
@@ -30,6 +30,8 @@ formulaire.addEventListener("submit", (event) => {
             localStorage.setItem("tokenIdentification",data.token)
             window.location.href = "http://127.0.0.1:5500" 
             
+            let barreNoireMofifier = document.querySelector("barreNoireMofifier")
+            barreNoireMofifier.classlist
             
         }else {
             alert ("Identifiant ou mot de passe incorrect")
