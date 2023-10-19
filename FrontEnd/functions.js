4ca6c1784992fe0de99264e14e67afc468dd688f
Cedric Travanca <cedric.travanca@gmail.com>
2023-10-19 18:28:21 +0200

    changement de toutes les fonctinos depuis script vers functions.js

--- FrontEnd/functions.js
+++ FrontEnd/functions.js
@@ -31,3 +31,237 @@ function addCategorieToSelect() {
         })
 }
 
+function showWorks() {
+    // Récupération des travaux qui vont apparaitre sans avoir cliquer sur les boutons de tri
+    fetch("http://localhost:5678/api/works")
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+
+            //on parcours la liste des éléments id présents dans data
+            for (let id in data) {
+                let object = data[id]
+
+                //Création d'une balise figure qui contiendras l'image et sa description
+                let figElement = document.createElement("figure")
+
+                let imgElement = document.createElement("img")
+                imgElement.src = object.imageUrl
+                imgElement.alt = object.title
+
+                let figCaptionElement = document.createElement("figCaption")
+                figCaptionElement.textContent = object.title
+
+
+                figElement.appendChild(imgElement)
+                figElement.appendChild(figCaptionElement)
+
+                //l'ensemble des éléments crées dans figElement vont être placés dans ma div gallery et se nommerons travaux
+                let travaux = document.querySelector(".gallery")
+                travaux.appendChild(figElement)
+            }
+        })
+}
+
+//Cette fonction permet d'ouvrir la boite modal et la mise en place des options qui l'accompagnent
+const openModal = function (e) {
+    e.preventDefault()
+    const target = document.querySelector(e.target.getAttribute("href"))
+    target.style.display = null
+    target.setAttribute("aria-modal", "true")
+    target.removeAttribute("aria-hidden")
+    modal = target
+    modal.addEventListener("click",closeModal)
+    modal.querySelector(".js-modal-close").addEventListener("click", closeModal)
+    modal.querySelector(".js-modal-stop").addEventListener("click", stopPropagation)
+
+}
+
+//Cette fonction permet de fermet la boite modal et la suppression des options qui l'accompagnent
+const closeModal = function (e) {
+    if (modal === null) return
+    e.preventDefault()
+    modal.style.display = "none"
+    modal.setAttribute("aria-hidden", "true")
+    modal.removeAttribute("aria-modal")
+    modal.removeEventListener("click",closeModal)
+    modal.querySelector(".js-modal-close").removeEventListener("click", closeModal)
+    modal.querySelector(".js-modal-stop").removeEventListener("click", stopPropagation)
+    modal = null
+}
+
+//fermeture de la modal en cas de pression sur la touche ESC du clavier
+window.addEventListener("keydown", function (e) {
+    if (e.key === "Escape" || e.key === "Esc") {
+        closeModal2(e)
+        closeModal(e)
+    }
+})
+
+//ouverture de la modal2 et des options qui l'accompagnent 
+const openModal2 = function (e) {
+    e.preventDefault()
+    const target = document.querySelector(e.target.getAttribute("href"))
+    target.style.display = null
+    target.setAttribute("aria-modal", "true")
+    target.removeAttribute("aria-hidden")
+    modal2 = target
+    modal2.addEventListener("click",closeModal2)
+    modal2.querySelector(".js-modal-close").addEventListener("click", closeModal2)
+    modal2.querySelector(".js-modal-stop").addEventListener("click", stopPropagation)
+    checkForm()
+}
+
+//fermeture de la boite modal2 et la suppression des options qui l'accompagnent
+const closeModal2 = function (e) {
+    modal2 = document.getElementById("modal2")
+    if (modal2 === null) return
+    modal2.style.display = "none"
+    modal2.setAttribute("aria-hidden", "true")
+    modal2.removeAttribute("aria-modal")
+    modal2.addEventListener("click",closeModal2)
+    modal2.querySelector(".js-modal-close").removeEventListener("click", closeModal2)
+    modal2.querySelector(".js-modal-stop").removeEventListener("click", stopPropagation)
+    modal2 = null
+    hidePreview()
+}
+
+//s'assurer que tout les champs soient vides en cas de retour de la modal2 sur la modal1 pour revenir de nouveau sur la modal2
+function clearModal2() {
+    if (document.getElementById("titre").value != '' || document.getElementById("categorie").value != '' || document.getElementById("id-input").value != ''){
+        document.getElementById("categorie").value = ''
+        document.getElementById("titre").value=''
+        document.getElementById("id-input").value = ''
+    }
+}
+
+//Supprimer une image en envoyant l'info à l'API
+function deleteImg(event) {
+    console.log(event.target.id)
+    fetch(`http://localhost:5678/api/works/${event.target.id}`, {
+        method: `DELETE`,
+        headers: { "Authorization": "Bearer " + sessionStorage.getItem("tokenIdentification") },
+    }).then(() => {
+        document.querySelector(".modal-img").innerHTML = "";
+        document.querySelector(".gallery").innerHTML = "";
+        showWorks()
+        loadImgModal()
+    })
+}
+
+//fonction permettant d'afficher dans la modal tout les travaux présents dans l'API
+async function loadImgModal() {
+    response = await fetch("http://localhost:5678/api/works")
+        .then(response => response.json())
+        .then(data => {
+
+            //on parcours la liste des éléments id présents dans data
+            for (let id in data) {
+                let object = data[id]
+
+                //Création d'une balise figure qui contiendras l'image et sa description
+                let figElement = document.createElement("figure")
+
+                let imgElement = document.createElement("img")
+                imgElement.src = object.imageUrl
+                imgElement.alt = object.title
+
+                //Création bouton poubelle sur les photos de la modal
+                let poubelle = document.createElement('i')
+
+                poubelle.className = "fa-solid fa-trash-can"
+                poubelle.id = object.id
+
+                //fonction deleteImg pour le click sur la corbeille
+                figElement.appendChild(poubelle)
+                poubelle.addEventListener("click", function (event) {
+                    deleteImg(event)
+                })
+
+                let figCaptionElement = document.createElement("figCaption")
+                figCaptionElement.textContent = object.title
+
+                figElement.appendChild(imgElement)
+
+                //l'ensemble des éléments crées dans figElement vont être placés dans la div modal-img et se nommerons travaux
+                let travaux = document.querySelector(".modal-img")
+                travaux.appendChild(figElement)
+            }
+        })
+}
+
+//fonction permettant d'envoyer les nouveaux projets à l'API
+async function loadNewWorks() {
+    let newImgCat = document.getElementById("categorie").value
+    let newImgTitle = document.getElementById("titre").value
+    let newImgImage = document.getElementById("id-input").files[0]
+    const formData = new FormData()
+    formData.append("image", newImgImage)
+    formData.append("title", newImgTitle)
+    formData.append("category", newImgCat)
+    const response = await fetch('http://localhost:5678/api/works/', {
+        method: 'POST',
+        headers: {
+            'Authorization': 'Bearer ' + sessionStorage.getItem("tokenIdentification")
+        },
+        body: formData
+    }).catch(err => console.log(err))
+    //Nettoyer la gallerie ainsi que la modal, puis les recharger avec les données présentes dans l'API
+    if (response.ok === true) {
+        document.querySelector(".modal-img").innerHTML = "";
+        document.querySelector(".gallery").innerHTML = "";
+        showWorks()
+        loadImgModal()
+        closeModal2()
+        alert("Le work à correctement été ajouté")
+
+    } else {
+        alert("Erreur lors de l'ajout d'un nouveau work")
+        throw new Error("Une erreur est survenue")
+    }
+}
+
+//affichage de la miniature de l'image choisis dans la modal2
+function showPreview(event) {
+    if (event.target.files.length > 0) {
+        let src = URL.createObjectURL(event.target.files[0])
+        let preview = document.getElementById("preview-new-image")
+        preview.src = src
+        preview.style.display = "block"
+        let champAjoutImg = document.getElementById("btn-add-img")
+        let champSize = document.querySelector(".max-size-img")
+        champAjoutImg.style.display = "none"
+        champSize.style.display = "none"
+        let champLogo = document.querySelector(".fa-regular.fa-image")
+        champLogo.style.display = "none"
+    }
+}
+
+//Effacer l'affichage de la miniature en cas de retour ou de cloture de la modal2
+function hidePreview() {
+        let preview = document.getElementById("preview-new-image")
+        preview.style.display = "none"
+        let champAjoutImg = document.getElementById("btn-add-img")
+        let champSize = document.querySelector(".max-size-img")
+        champAjoutImg.style.display = "flex"
+        champSize.style.display = "flex"
+        let champLogo = document.querySelector(".fa-regular.fa-image")
+        champLogo.style.display = "flex"
+}
+
+//Permet de verifier si tout les champs du formulaire sont remplis.
+function checkForm() {
+    let newImgCat = document.getElementById("categorie").value
+    let newImgTitle = document.getElementById("titre").value
+    let newImgImage = document.getElementById("id-input").value
+
+    btnValiderNouvelleImg = document.getElementById("valider-nouvelle-img")
+    // Si les champs sont remplis, le bouton de validation s'active
+    if (newImgTitle !== '' && newImgCat !== '' && newImgImage !== '') {
+        btnValiderNouvelleImg.disabled = false;
+    }
+    //si un champ n'est pas remplis, le bouton de validation ne s'active pas
+    else {
+        btnValiderNouvelleImg.disabled = true;
+    }
+}
--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -21,7 +21,6 @@ sectFiltre.appendChild(btnTrierTous)
 
 
 
-
 //nous appelons les catégories via fetch afin de les utiliser pour nos filtres
 fetch("http://localhost:5678/api/categories")
     .then(response => response.json())
@@ -104,222 +103,69 @@ fetch("http://localhost:5678/api/categories")
 
     })
 
-function showWorks() {
-    // Récupération des travaux qui vont apparaitre sans avoir cliquer sur les boutons de tri
-    fetch("http://localhost:5678/api/works")
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-
-            //on parcours la liste des éléments id présents dans data
-            for (let id in data) {
-                let object = data[id]
-
-                //Création d'une balise figure qui contiendras l'image et sa description
-                let figElement = document.createElement("figure")
-
-                let imgElement = document.createElement("img")
-                imgElement.src = object.imageUrl
-                imgElement.alt = object.title
-
-                let figCaptionElement = document.createElement("figCaption")
-                figCaptionElement.textContent = object.title
-
-
-                figElement.appendChild(imgElement)
-                figElement.appendChild(figCaptionElement)
-
-                //l'ensemble des éléments crées dans figElement vont être placés dans ma div gallery et se nommerons travaux
-                let travaux = document.querySelector(".gallery")
-                travaux.appendChild(figElement)
-            }
-        })
-}
 showWorks()
 
-
+//Bandeau noir du mode edition qui disparait si nous ne somme pas enregistrés
 headband = document.querySelector(".black-headband")
 headband.style.display = "none"
 
+//Bouton modifier des projets qui disparait si nous ne somme pas enregistrés ce qui fait que la modal est fermée
 btnModif = document.getElementById("open-modal")
 btnModif.style.display = "none"
-
+//La deuxieme modal est fermée
 btnOpenModal2 = document.getElementById("modal2")
 btnOpenModal2.style.display = "none"
 
-
+//si nous sommes connectés, changement de certains parametres
 if (isConnect == true) {
-    console.log("connecté")
+    //changer login en logout
     showLogout = document.getElementById("login")
     showLogout.textContent = "logout"
     logoutId = document.getElementById("login")
     logoutId.id = "logout"
+    //effacer le storage du token si il y a un clic sur "Logout"
     deleteTokenStorage = document.getElementById("logout")
     deleteTokenStorage.addEventListener("click", function () {
         sessionStorage.removeItem("tokenIdentification")
         window.location.href = "http://127.0.0.1:5500/index.html"
     })
-
+//Le bandeau noir du mode édition apparait si nous sommes connectés et les filtres disparraissent
     headband.style.display = "flex"
     btnModif.style.display = "inline-block"
     filters = document.querySelector(".filters")
     filters.style.display = "none"
 
 } else {
-    console.log("déconnecté")
+    //si nous ne sommes pas connectés, le bouton de modification n'apparait pas
     btnModif.style.display = "none"
 }
 
 let modal = null
 
-const openModal = function (e) {
-    e.preventDefault()
-    const target = document.querySelector(e.target.getAttribute("href"))
-    target.style.display = null
-    target.setAttribute("aria-modal", "true")
-    target.removeAttribute("aria-hidden")
-    modal = target
-    modal.addEventListener("click",closeModal)
-    modal.querySelector(".js-modal-close").addEventListener("click", closeModal)
-    modal.querySelector(".js-modal-stop").addEventListener("click", stopPropagation)
-
-}
-
-const closeModal = function (e) {
-    if (modal === null) return
-    e.preventDefault()
-    modal.style.display = "none"
-    modal.setAttribute("aria-hidden", "true")
-    modal.removeAttribute("aria-modal")
-    modal.removeEventListener("click",closeModal)
-    modal.querySelector(".js-modal-close").removeEventListener("click", closeModal)
-    modal.querySelector(".js-modal-stop").removeEventListener("click", stopPropagation)
-    modal = null
-}
-
 const stopPropagation = function (e) {
     e.stopPropagation()
 }
 
-//piverture de la modal lors du click sur le bouton "modifier" de la page principal
+//ouverture de la modal lors du click sur le bouton "modifier" de la page principal
 let clickModal = document.querySelector(".js-modal")
 clickModal.addEventListener("click", openModal)
 
-//fermeture de la modal en cas de pression sur la touche ESC du clavier
-window.addEventListener("keydown", function (e) {
-    if (e.key === "Escape" || e.key === "Esc") {
-        closeModal2(e)
-        closeModal(e)
-    }
-})
+
 
 let modal2 = null
-
-const openModal2 = function (e) {
-    e.preventDefault()
-    const target = document.querySelector(e.target.getAttribute("href"))
-    target.style.display = null
-    target.setAttribute("aria-modal", "true")
-    target.removeAttribute("aria-hidden")
-    modal2 = target
-    modal2.addEventListener("click",closeModal2)
-    modal2.querySelector(".js-modal-close").addEventListener("click", closeModal2)
-    modal2.querySelector(".js-modal-stop").addEventListener("click", stopPropagation)
-    checkForm()
-}
-
-const closeModal2 = function (e) {
-    modal2 = document.getElementById("modal2")
-    if (modal2 === null) return
-    modal2.style.display = "none"
-    modal2.setAttribute("aria-hidden", "true")
-    modal2.removeAttribute("aria-modal")
-    modal2.addEventListener("click",closeModal2)
-    modal2.querySelector(".js-modal-close").removeEventListener("click", closeModal2)
-    modal2.querySelector(".js-modal-stop").removeEventListener("click", stopPropagation)
-    modal2 = null
-    hidePreview()
-}
-
-//s'assurer que tout les champs soient vides en cas de retour sur la modal1 pour revenir sur la modal2
-function clearModal2() {
-    if (document.getElementById("titre").value != '' || document.getElementById("categorie").value != '' || document.getElementById("id-input").value != ''){
-        document.getElementById("categorie").value = ''
-        document.getElementById("titre").value=''
-        document.getElementById("id-input").value = ''
-    }
-}
 
 //lors du click sur "ajouter une photo" la modal2 s'ouvre et avec tout les champs vides
 let clickModal2 = document.getElementById("open-modal2")
 clickModal2.addEventListener("click", openModal2)
 clickModal2.addEventListener("click", clearModal2)
 
+//lors du clic sur la fleche de retour de la modal2, retour sur la modal
 let backToModal = document.querySelector(".fa-arrow-left")
 backToModal.addEventListener("click", closeModal2)
 
-
-
 addCategorieToSelect()
 
-//Supprimer une image en envoyant l'info à l'API
-function deleteImg(event) {
-    console.log(event.target.id)
-    fetch(`http://localhost:5678/api/works/${event.target.id}`, {
-        method: `DELETE`,
-        headers: { "Authorization": "Bearer " + sessionStorage.getItem("tokenIdentification") },
-    }).then(() => {
-        document.querySelector(".modal-img").innerHTML = "";
-        document.querySelector(".gallery").innerHTML = "";
-        showWorks()
-        loadImgModal()
-    })
-
-}
-
-
 loadImgModal()
-//fonction permettant d'afficher dans la modal tout les travaux présents dans l'API
-async function loadImgModal() {
-    response = await fetch("http://localhost:5678/api/works")
-        .then(response => response.json())
-        .then(data => {
-
-            //on parcours la liste des éléments id présents dans data
-            for (let id in data) {
-                let object = data[id]
-
-                //Création d'une balise figure qui contiendras l'image et sa description
-                let figElement = document.createElement("figure")
-
-                let imgElement = document.createElement("img")
-                imgElement.src = object.imageUrl
-                imgElement.alt = object.title
-
-                //Création bouton poubelle sur les photos de la modal
-                let poubelle = document.createElement('i')
-
-                poubelle.className = "fa-solid fa-trash-can"
-                poubelle.id = object.id
-
-                //fonction deleteImg pour le click sur la corbeille
-                figElement.appendChild(poubelle)
-                poubelle.addEventListener("click", function (event) {
-                    deleteImg(event)
-                })
-
-                let figCaptionElement = document.createElement("figCaption")
-                figCaptionElement.textContent = object.title
-
-                figElement.appendChild(imgElement)
-
-                //l'ensemble des éléments crées dans figElement vont être placés dans la div modal-img et se nommerons travaux
-                let travaux = document.querySelector(".modal-img")
-                travaux.appendChild(figElement)
-
-            }
-        })
-}
 
 //afficher les nouveaux travaux après validation, sans recharger la page
 let btnValiderNouvelleImg = document.getElementById("form")
@@ -328,61 +174,8 @@ btnValiderNouvelleImg.addEventListener("submit", function (event) {
     loadNewWorks()
 })
 
-//fonction permettant d'envoyer les nouveaux projets à l'API
-async function loadNewWorks() {
-    let newImgCat = document.getElementById("categorie").value
-    let newImgTitle = document.getElementById("titre").value
-    let newImgImage = document.getElementById("id-input").files[0]
-    const formData = new FormData()
-    formData.append("image", newImgImage)
-    formData.append("title", newImgTitle)
-    formData.append("category", newImgCat)
-    const response = await fetch('http://localhost:5678/api/works/', {
-        method: 'POST',
-        headers: {
-            'Authorization': 'Bearer ' + sessionStorage.getItem("tokenIdentification")
-        },
-        body: formData
-    }).catch(err => console.log(err))
-    //Nettoyer la gallerie ainsi que la modal, puis les recharger avec les données présentes dans l'API
-    if (response.ok === true) {
-        document.querySelector(".modal-img").innerHTML = "";
-        document.querySelector(".gallery").innerHTML = "";
-        showWorks()
-        loadImgModal()
-        closeModal2()
-        alert("Le work à correctement été ajoutés")
 
-    } else {
-        alert("Erreur lors de l'ajout d'un nouveau work")
-        throw new Error("Une erreur est survenue")
-    }
-}
 
-function showPreview(event) {
-    if (event.target.files.length > 0) {
-        let src = URL.createObjectURL(event.target.files[0])
-        let preview = document.getElementById("preview-new-image")
-        preview.src = src
-        preview.style.display = "block"
-        let champAjoutImg = document.getElementById("btn-add-img")
-        let champSize = document.querySelector(".max-size-img")
-        champAjoutImg.style.display = "none"
-        champSize.style.display = "none"
-        let champLogo = document.querySelector(".fa-regular.fa-image")
-        champLogo.style.display = "none"
-    }
-}
-function hidePreview() {
-        let preview = document.getElementById("preview-new-image")
-        preview.style.display = "none"
-        let champAjoutImg = document.getElementById("btn-add-img")
-        let champSize = document.querySelector(".max-size-img")
-        champAjoutImg.style.display = "flex"
-        champSize.style.display = "flex"
-        let champLogo = document.querySelector(".fa-regular.fa-image")
-        champLogo.style.display = "flex"
-}
 
 let newImgTitle = document.getElementById("titre")
 let newImgCat = document.getElementById("categorie")
@@ -392,18 +185,5 @@ newImgTitle.addEventListener("change", checkForm)
 newImgCat.addEventListener("change", checkForm)
 newImgImage.addEventListener("change", checkForm)
 
-function checkForm() {
-    let newImgCat = document.getElementById("categorie").value
-    let newImgTitle = document.getElementById("titre").value
-    let newImgImage = document.getElementById("id-input").value
 
-    btnValiderNouvelleImg = document.getElementById("valider-nouvelle-img")
-    if (newImgTitle !== '' && newImgCat !== '' && newImgImage !== '') {
-        console.log(newImgTitle.value)
-        btnValiderNouvelleImg.disabled = false;
-    }
-    else {
-        btnValiderNouvelleImg.disabled = true;
-    }
-}
 
